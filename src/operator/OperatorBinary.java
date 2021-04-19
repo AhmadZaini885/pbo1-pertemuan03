d07b7b3294d833bfa5743c2fb2a1b084b1148816
ahmadzaini885 <ahmadzaini0703@gmail.com>
2021-04-19 22:05:39 +0800

    Operator Binary Assigment

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -2,7 +2,7 @@ package operator;
 
 public class OperatorBinary {
 
-    String nama = "Ahmad Zaini"
+    String nama = "Ahmad Zaini";
     int jumlahSks = 24;
     double IndexkPrestasiKumulatif = 3.90;
 
